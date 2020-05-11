a1a64e38fecdf8da52c0ccb0374c297608652c04
Mariya <mariya.podosinova@gmail.com>
2020-05-11 09:50:07 +0100

    Pokecard component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import React from "react";
-import Pokedex from "./Pokedex";
+import React, { Component } from "react";
+
+import Pokecard from "./Pokecard";
 
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <Pokedex />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Pokecard
+          id={4}
+          name={"Charmander"}
+          type={"fire"}
+          base_experience={62}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,15 +1,25 @@
-import React from "react";
-import "./Pokerdex.css";
+import React, { Component } from "react";
+import "./Pokecard.css";
 
-let Pokemon = [
-  { id: 4, name: "Charmander", type: "fire", base_experience: 62 },
-  { id: 7, name: "Squirtle", type: "water", base_experience: 63 },
-  { id: 11, name: "Metapod", type: "bug", base_experience: 72 },
-  { id: 12, name: "Butterfree", type: "flying", base_experience: 178 },
-  { id: 25, name: "Pikachu", type: "electric", base_experience: 112 },
-  { id: 39, name: "Jigglypuff", type: "normal", base_experience: 95 },
-  { id: 94, name: "Gengar", type: "poison", base_experience: 225 },
-  { id: 133, name: "Eevee", type: "normal", base_experience: 65 },
-];
+class Pokecard extends Component {
+  render() {
+    return (
+      <div className="Pokecard">
+        <h1>{this.props.name}</h1>
+      </div>
+    );
+  }
+}
 
-export default Pokermon;
+// let Pokemon = [
+//   { id: 4, name: "Charmander", type: "fire", base_experience: 62 },
+//   { id: 7, name: "Squirtle", type: "water", base_experience: 63 },
+//   { id: 11, name: "Metapod", type: "bug", base_experience: 72 },
+//   { id: 12, name: "Butterfree", type: "flying", base_experience: 178 },
+//   { id: 25, name: "Pikachu", type: "electric", base_experience: 112 },
+//   { id: 39, name: "Jigglypuff", type: "normal", base_experience: 95 },
+//   { id: 94, name: "Gengar", type: "poison", base_experience: 225 },
+//   { id: 133, name: "Eevee", type: "normal", base_experience: 65 },
+// ];
+
+export default Pokecard;
