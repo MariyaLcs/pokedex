71d6eb1233f22d453764ce0c4bbbcef6a2d33a85
Mariya <mariya.podosinova@gmail.com>
2020-05-11 12:45:51 +0100

    Updates

---
+++ precache-manifest.8423ad705443cbd9118d3b4442b1854d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97c928c3855cffc3027407c13ada35a3",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "b3f7aaab2b1f38368344",
+    "url": "/pokedex/static/css/main.bee482de.chunk.css"
+  },
+  {
+    "revision": "a041565f949ffbf8e421",
+    "url": "/pokedex/static/js/2.b5c1536f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokedex/static/js/2.b5c1536f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3f7aaab2b1f38368344",
+    "url": "/pokedex/static/js/main.1c006604.chunk.js"
+  },
+  {
+    "revision": "014345e0bd0e463ebfd2",
+    "url": "/pokedex/static/js/runtime-main.83eb840c.js"
+  }
+]);
